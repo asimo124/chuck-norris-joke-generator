0f2f8d955c7ce996f5361071ed12288c97f97dbd
Alex Hawley <asimo124@yahoo.com>
2019-04-11 23:44:31 -0500

    First Commit.

---
+++ src/main/java/info/alexhawley/cnjokegenerator/CnJokeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package info.alexhawley.cnjokegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnJokeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnJokeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/info/alexhawley/cnjokegenerator/services/JokeService.java
@@ -0,0 +1,6 @@
+package info.alexhawley.cnjokegenerator.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
